44ba3c5137334a02514a684cf3932593e228098b
LemKin <kevinsimon.pro@gmail.com>
2021-11-22 16:48:38 +0100

    crypto

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,13 +1,14 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const crypto = require('crypto-js'); //pour chiffrer l'email en cas de piratage de la base de données
 const {DEFAULT_TOKEN_KEY} = require('../config/auth');
 
 exports.signup = (req, res, next) => {
   bcrypt.hash(req.body.password, 10)
     .then(hash => {
       const user = new User({
-        email: req.body.email,
+        email: crypto.SHA256(req.body.email), //algorithme de hachage sécurisé 256 bits
         password: hash
       });
       user.save()
@@ -19,7 +20,7 @@ exports.signup = (req, res, next) => {
 
 exports.login = (req, res, next) => {
   console.log(req.body);
-  User.findOne({ email: req.body.email })
+  User.findOne({ email: crypto.SHA256(req.body.email).toString(crypto.enc.Hex) }) //toString pour convertir le tableau renvoyé par crypto en string
           .then(user => {
           if (!user) {
               return res.status(401).json({ error: 'Utilisateur non trouvé !' });
